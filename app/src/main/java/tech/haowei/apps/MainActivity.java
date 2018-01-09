4d5909c2f3043f2b41f547d9e54d830c31953354
haowei4032 <haowei4032@126.com>
2018-01-09 19:20:46 +0800

    initialize apps kenrel

--- app/src/main/java/tech/haowei/apps/MainActivity.java
+++ app/src/main/java/tech/haowei/apps/MainActivity.java
@@ -159,8 +159,15 @@ public class MainActivity extends AppCompatActivity {
 
                     // View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
                     Window window = getWindow();
+
                     window.addFlags(WindowManager.LayoutParams
-                            .FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                            .FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+                    if (color.equals("black")) {
+                        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+                    } else {
+                        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                    }
 
                     window.setStatusBarColor(Color.parseColor(color));
                     navigatorView.setBackgroundColor(Color.parseColor(color));
